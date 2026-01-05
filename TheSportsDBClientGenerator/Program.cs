465ed27451a4f41d2253ab5eed8f32d7764d6caa
Sergey Bizin <bizin@listen2u.ru>
2026-01-05 08:24:29 +0700

    Add client generator

---
+++ TheSportsDBClientGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿string openapiV1YamlName = "InternalOpenapiV1";
+string openapiV1ClassPath = $"../TheSportsDB/{openapiV1YamlName}Client.cs";
+
+File.Move($"obj/{openapiV1YamlName}Client.cs", openapiV1ClassPath, true);
+
+ReplaceInFile(openapiV1ClassPath, "    public partial class OpenapiV1Client", "    internal partial class OpenapiV1Client");
+
+void ReplaceInFile(string filePath, string oldText, string newText)
+{
+    string fileContent = File.ReadAllText(filePath);
+    string modifiedContent = fileContent.Replace(oldText, newText);
+
+    File.WriteAllText(filePath, modifiedContent);
+}
